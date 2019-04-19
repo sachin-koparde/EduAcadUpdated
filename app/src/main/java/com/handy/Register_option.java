5a9243f3d8f62f46ce056a1e7be2829efd9a4364
Sach <navodatasacchu26@gmail.com>
2019-04-19 21:37:53 +0530

    Saved

---
+++ app/src/main/java/com/handy/Register_option.java
@@ -0,0 +1,13 @@
+package com.handy;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Register_option extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register_option);
+    }
+}
